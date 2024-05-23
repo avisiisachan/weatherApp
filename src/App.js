967e9a31588190817396cf93008ea7a05f9a8c90
Avisi Sachan <avisachan07@.com>
2024-05-23 20:57:34 +0530

    completedfrontend

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import Input from './Components/Input';
 import TimeAndLocation from './Components/TimeAndLocation';
 import TempAndDetails from './Components/TempAndDetails';
 import Forecast from './Components/Forecast';
-import { getWeatherData } from './Helpers/weatherHelpers';
+import getFormattedWeatherData from './Helpers/weatherHelpers'
 
 function App() {
-  const getWeather=async ()=> {
-    const data= await getWeatherData('weather', {q: 'berlin'})
+  const getWeather= async ()=> {
+    const data= await getFormattedWeatherData({q: 'berlin'})
+    console.log(data);
   }
+
+  getWeather();
   
   return (
     <div className="mx-auto max-w-screen-lg mt-4 py-5 px-4 sm:px-6 md:px-12 lg:px-24 xl:px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-400 to-blue-600">
--- src/Helpers/weatherHelpers.js
+++ src/Helpers/weatherHelpers.js
@@ -1,9 +1,42 @@
+import { DateTime } from "luxon";
+
 const API_KEY ='4af65889b66498e3d09431b407cbb9df'
 const BASE_URL= 'https://api.openweathermap.org/data/2.5/'
 
-export  const getWeatherData= (infoType, searchParams)=>{
+
+const getWeatherData= (infoType, searchParams)=>{
     const url= new URL(BASE_URL + infoType);
     url.search= new URLSearchParams({...searchParams, appid: API_KEY});
 
-    return fetch(url).then((res)=> res.json()).then((data)=>data);
+    return fetch(url).then((res)=> res.json());
+};
+
+const formatToLocalTime=(secs, offset, format="cccc, dd LLL yyyy' | Locat time: 'hh:mm a")=>{
+    return DateTime.fromSeconds(secs +offset, {zone:'utc'}).toFormat(format);
+};
+
+const iconUrlFromCode=(icon)=>{
+    return `http://openweathermap.org/img/wn/${icon}@2x.png`;
 }
+
+const formatCurrent=(data)=>{
+    const {coord:{lat, lon}, main:{temp, feels_like, temp_min, temp_max, humidity}, name, dt, sys:{country, sunrise, sunset}, weather, wind:{speed}, timezone}=data;
+
+    const {main:details, icon}=weather[0];
+    const formattedLocalTime=formatToLocalTime(dt, timezone);
+
+    return{temp, feels_like, temp_min, temp_max,humidity, name, country, sunrise: formatToLocalTime(sunrise, timezone, 'hh:mm a'), sunset: formatToLocalTime(sunset, timezone, 'hh:mm a'), speed, details, icon: iconUrlFromCode(icon), formattedLocalTime, dt, timezone, lat, lon};
+};
+
+const getFormattedweatherData= async(searchParams)=>{
+    const formattedCurrentWeather= await getWeatherData('weather', searchParams).then(formatCurrent);
+
+
+    const{dt, lat, lon, timezone}=formattedCurrentWeather;
+
+    const formattedForecastWeather= await getWeatherData('forecast', {lat, lon, units: searchParams.units}).then((d)=> forma)
+
+    return {...formattedCurrentWeather}
+};
+
+export default getFormattedweatherData;
